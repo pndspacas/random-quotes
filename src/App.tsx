2f8fd956905bbb74371a3b627714e5773322af00
Pedro <pndspacas@gmail.com>
2023-10-26 19:06:24 -0700

    added

--- src/App.tsx
+++ src/App.tsx
@@ -7,24 +7,30 @@ function App() {
   const [quote, setQuotes] = useState([])
   const iconStyle = { color: "#6497b5" }
   const newQuote = async () => {
-    const random = Math.floor(Math.random() * 100)
+    const random = Math.floor(Math.random() * 16) + 1
     const res = await axios.get("https://type.fit/api/quotes")
-    setQuotes(res.data[random])
+    const newQuoteData = res.data[random]
+    setQuotes(newQuoteData)
+    console.log(newQuoteData)
   }
 
   useEffect(() => {
     newQuote()
   }, [])
 
+
   return (
     <>
       <div className='container'>
         <div className="box">
           {
-            quote.author === null
-              ? <p><FaQuoteLeft style={iconStyle} /> {quote.text} <FaQuoteRight style={iconStyle} /><br /><span>- Author Unknown</span></p>
-              : <p><FaQuoteLeft style={iconStyle} /> {quote.text} <FaQuoteRight style={iconStyle} /><br /><span> - {quote.author}</span></p>
+            quote && (
+              quote.author === "type.fit"
+                ? <p><FaQuoteLeft style={iconStyle} /> {quote.text} <FaQuoteRight style={iconStyle} /><br /><span>- Author Unknown</span></p>
+                : <p><FaQuoteLeft style={iconStyle} /> {quote.text} <FaQuoteRight style={iconStyle} /><br /><span> - {quote.author.replace("type.fit", "").split(",")}</span></p>
+            )
           }
+
           <div className="button">
             <button onClick={newQuote}>Generate Quote</button>
           </div>
